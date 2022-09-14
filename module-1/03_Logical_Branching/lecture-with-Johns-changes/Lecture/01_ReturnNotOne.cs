a90aa3a2d65cdbeeec964b058c4aa06a01089d96
John Fulton <john@techelevator.com>
2022-09-14 16:53:22 -0400

    module-1/03_Logical_Branching/ with in-class changes

---
+++ module-1/03_Logical_Branching/lecture-with-Johns-changes/Lecture/01_ReturnNotOne.cs
@@ -0,0 +1,16 @@
+﻿namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        1. This method is named ReturnNotOne and it returns an int.
+        Change it so that it still returns an int but something other than 1.
+        TOPIC: Method Signatures & Tests
+        */
+        public int ReturnNotOne()
+        {
+            int x = 2;
+            return x;
+        }
+    }
+}
---
+++ module-1/03_Logical_Branching/lecture-with-Johns-changes/Lecture/15_ReturnAdultOrMinorAgain.cs
@@ -0,0 +1,22 @@
+﻿namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        15. Now, do it again with a different bool 
+        opeation.
+        TOPIC: Logical Not
+        */
+        public string ReturnAdultOrMinorAgain(int number)
+        {
+            if (!(number < 18))
+            {
+                return "Adult";
+            }
+            else
+            {
+                return "Minor";
+            }
+        }
+    }
+}
