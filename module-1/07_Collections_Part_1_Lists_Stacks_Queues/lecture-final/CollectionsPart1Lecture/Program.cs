694a2912ace50eb37a5b35fffe7be01dde6a5566
John Fulton <john@techelevator.com>
2022-09-20 12:39:36 -0400

    module-1/07_Collections_Part_1_Lists_Stacks_Queues/ with in-class changes

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture-final/CollectionsPart1Lecture/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPart1Lecture
+{
+    public class CollectionsPart1Lecture
+	{
+        static void Main(string[] args)
+        {
+			Console.WriteLine("####################");
+			Console.WriteLine("       LISTS");
+			Console.WriteLine("####################");
+
+			List<string> names = new List<string>();
+			names.Add("Frodo");
+			names.Add("Sam");
+			names.Add("Pippin");
+			names.Add("Merry");
+			names.Add("Gandalf");
+			names.Add("Aragorn");
+			names.Add("Boromir");
+			names.Add("Gimli");
+			names.Add("Legolas");
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists are ordered");
+			Console.WriteLine("####################");
+
+			//the elements will be returned in the same order they were added
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists allow duplicates");
+			Console.WriteLine("####################");
+
+			names.Add("Sam");
+
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists allow elements to be inserted in the middle");
+			Console.WriteLine("####################");
+
+			names.Insert(2, "David");
+
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists allow elements to be removed by index");
+			Console.WriteLine("####################");
+
+			names.RemoveAt(2);
+
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Find out if something is already in the List");
+			Console.WriteLine("####################");
+
+			bool inList = names.Contains("Samwise");
+			Console.WriteLine("Samwise is in the list of names: " + inList);
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Find index of item in List");
+			Console.WriteLine("####################");
+
+			int indexOfGandalf = names.IndexOf("Gandalf");
+			Console.WriteLine("Gandalf is located at index: " + indexOfGandalf);
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists can be turned into an array");
+			Console.WriteLine("####################");
+
+			string[] namesArray = names.ToArray();
+			for (int i = 0; i < namesArray.Length; i++)
+			{
+				Console.WriteLine(namesArray[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists can be sorted");
+			Console.WriteLine("####################");
+
+			names.Sort(); //Sort() modifies the original list, doesn't require assignment
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("Lists can be reversed too");
+			Console.WriteLine("####################");
+
+			names.Reverse(); //Reverse() modifies the original list, doesn't require assignment
+			for (int i = 0; i < names.Count; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			Console.WriteLine("####################");
+			Console.WriteLine("       FOREACH");
+			Console.WriteLine("####################");
+			Console.WriteLine();
+
+			// Let's loop through names again, but this time using a foreach loop
+			// for each name in names
+			foreach (string name in names)
+			{
+				// print the name
+				Console.WriteLine(name);
+			}
+		}
+	}
+}
---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture-with-johns-changes/CollectionsPart1Lecture/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPart1Lecture
+{
+    public class CollectionsPart1Lecture
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("####################");
+            Console.WriteLine("       LISTS");
+            Console.WriteLine("####################");
+
+            List<int> myInts = new List<int>();
+
+            List<string> names = new List<string> { "Matt", "John", "Steve", "Brian" };
+
+            Console.WriteLine(myInts.Count);
+
+            Console.WriteLine(names.Count);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine();
+
+
+
+            names.Add("Scott");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine(names.Count);
+
+            Console.WriteLine();
+
+
+
+            names.Insert(0, "Laura");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine(names.Count);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists are ordered");
+            Console.WriteLine("####################");
+
+            int position = names.IndexOf("Scott");
+            names.RemoveAt(position);
+            names.Insert(0, "Scott");
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine(names.Count);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists allow duplicates");
+            Console.WriteLine("####################");
+
+            names.Add("John");
+
+            Display(names);
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists allow elements to be inserted in the middle");
+            Console.WriteLine("####################");
+
+            names.Insert(names.Count / 2, "About the middle");
+            Display(names);
+
+
+            Console.WriteLine("####################");
+
+            Console.WriteLine("Lists allow elements to be removed by index");
+            Console.WriteLine("####################");
+    
+            Console.WriteLine("####################");
+            Console.WriteLine("Find out if something is already in the List");
+            Console.WriteLine("####################");
+
+            position = names.IndexOf("Laura");
+            Console.WriteLine(position);
+
+            Console.WriteLine((position == -1) ? "Not found!" : "Good job!!");
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Find index of item in List");
+            Console.WriteLine("####################");
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists can be turned into an array");
+            Console.WriteLine("####################");
+
+            string[] result = names.ToArray();
+
+            foreach(string item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists can be sorted");
+            Console.WriteLine("####################");
+            names.Sort();
+            Display(names);
+
+            Console.WriteLine("####################");
+            Console.WriteLine("Lists can be reversed too");
+            Console.WriteLine("####################");
+            names.Reverse();
+            Display(names);
+
+            Console.WriteLine("####################");
+            Console.WriteLine("       FOREACH");
+            Console.WriteLine("####################");
+            Console.WriteLine();
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("       STACK");
+            Console.WriteLine("####################");
+            Console.WriteLine();
+
+
+            Stack<int> myStack = new Stack<int>();
+
+            myStack.Push(3);
+            myStack.Push(1);
+            myStack.Push(4);
+            myStack.Push(1);
+            myStack.Push(5);
+
+            Console.WriteLine(myStack.Count);
+
+            Console.WriteLine();
+            foreach(int item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            while (myStack.Count > 0)
+            {
+                Console.WriteLine("item is: " + myStack.Pop() + "  Lenght is: " + myStack.Count);
+            }
+
+
+            Console.WriteLine("####################");
+            Console.WriteLine("       QUEUE");
+            Console.WriteLine("####################");
+            Console.WriteLine();
+
+
+            Queue<string> myQueue = new Queue<string>();
+
+
+            myQueue.Enqueue("Primrose");
+            myQueue.Enqueue("Gabriel");
+            myQueue.Enqueue("Penny");
+            myQueue.Enqueue("Bella");
+
+            Console.WriteLine(myQueue.Count);
+
+            foreach(string pet in myQueue)
+            {
+                Console.WriteLine(pet);
+            }
+
+            string firstPet = myQueue.Dequeue();
+            Console.WriteLine(myQueue.Count);
+
+            foreach (string pet in myQueue)
+            {
+                Console.WriteLine(pet);
+            }
+
+        }
+
+        static void Display (List<string> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+            Console.WriteLine(items.Count);
+
+            Console.WriteLine();
+        }
+    }
+}
