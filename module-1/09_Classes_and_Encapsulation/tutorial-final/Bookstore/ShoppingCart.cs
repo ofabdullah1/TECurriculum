a6af7e307e8437f6e0aa717c14d795d19b298bb1
John Fulton <john@techelevator.com>
2022-09-21 08:00:14 -0400

    module-1/08_Collections_Part_2_Dictionaries/

---
+++ module-1/09_Classes_and_Encapsulation/tutorial-final/Bookstore/ShoppingCart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechElevator.Bookstore
+{
+    public class ShoppingCart
+    {
+        private List<Book> booksToBuy = new List<Book>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0.0M;
+                foreach (Book book in this.booksToBuy)
+                {
+                    total += book.Price;
+                }
+                return total;
+            }
+        }
+
+        public void Add(Book bookToAdd)
+        {
+            booksToBuy.Add(bookToAdd);
+        }
+
+        public string GetReceipt()
+        {
+            string receipt = "Receipt\n";
+            foreach (Book book in this.booksToBuy)
+            {
+                receipt += book.GetBookInfo();
+                receipt += "\n";
+            }
+            receipt += "\nTotal: $" + this.TotalPrice;
+            return receipt;
+        }
+    }
+}
---
+++ module-1/09_Classes_and_Encapsulation/tutorial/Bookstore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TechElevator.Bookstore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tech Elevator Bookstore");
+            Console.WriteLine();
+
+            // Step Three: Test the getters and setters
+
+            // Step Five: Test the Book constructor
+
+            // Step Nine: Test the ShoppingCart class
+
+        }
+    }
+}
