ef8e16d47f0dfe219eaf29c5e1830b99b56539af
John Fulton <john@techelevator.com>
2022-09-23 18:32:42 -0400

    module-1/10B_Optional_Weekend_Exercises/

---
+++ module-1/10B_Optional_Weekend_Exercises/Exercises/Has12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+
+
+        /*
+         Given an array of ints, return true if there is a 1 in the array 
+         with a 2 somewhere later in the array.
+         has12([1, 3, 2]) → true
+         has12([3, 1, 2]) → true
+         has12([3, 1, 4, 5, 2]) → true
+         */
+        public bool Has12(int[] nums)
+        {
+            return false;
+        }
+    }
+}
+
+//Stuck? Here is a solution - https://vimeo.com/501568556/374eb4795d
---
+++ module-1/10B_Optional_Weekend_Exercises/Exercises/More14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class Exercises
+    {
+        /*
+         Given an array of ints, return true if the number of 1's is 
+         greater than the number of 4's
+         more14([1, 4, 1]) → true
+         more14([1, 4, 1, 4]) → false
+         more14([1, 1]) → true
+         */
+        public bool More14(int[] nums)
+        {
+            return false;
+        }
+    }
+}
+
+// Stuck? - Here is a solution - https://vimeo.com/501882394/a44883058e
