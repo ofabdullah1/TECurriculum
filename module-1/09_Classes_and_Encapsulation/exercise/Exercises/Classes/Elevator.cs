8f8e04ab1b9474a925f4d545d1ee114103f09f13
Omar Abdullah <ofabdullah1@gmail.com>
2022-09-24 14:10:35 -0400

    optional exercise pull

--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Elevator.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Elevator.cs
@@ -3,6 +3,45 @@
     public class Elevator
     {
 
+        public int CurrentLevel { get; private set; } = 1;
+        public int NumberOfLevels { get; private set; } = 1;
+        public bool DoorIsOpen { get; private set; }
+
+
+        public void OpenDoor()
+        {
+
+        }
+
+
+        public void CloseDoor()
+        {
+
+        }
+
+
+        public void GoUp(int deiredFloor)
+        {
+
+        }
+
+
+
+        public void GoDown(int desiredFloor)
+        {
+
+        }
+
+
+        public Elevator(int numberOfLevels)
+        {
+            NumberOfLevels = numberOfLevels;
+
+        }
+
+
+
 
     }
+
 }
--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Television.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/Television.cs
@@ -2,48 +2,64 @@
 {
     public class Television
     {
-        public bool IsOn { get; private set; }
-        public int CurrentChannel { get; private set; }
-        public int CurrentVolume { get; private set; }
+        public bool IsOn { get; private set; } = false;
+        public int CurrentChannel { get; private set; } = 3;
+        public int CurrentVolume { get; private set; } = 2;
 
-        public void TurnOff()
-        {
-            IsOn = false;
-        }
+        
 
         public void TurnOn()
         {
             
-            IsOn = false;
-            while(!IsOn)
+           
+            if(!IsOn)
             {
                 IsOn = true;
             }
+            
         }
-        
+
+
+        public void TurnOff()
+        {
+           
+            if (IsOn)
+            {
+                IsOn = false;
+            }
+           
+        }
+
         public void ChangeChannel(int newChannel)
         {
-
+            
         }
 
         public void ChannelUp()
         {
-
+            
         }
 
         public void ChannelDown()
         {
-
+           
         }
 
         public void RaiseVolume()
         {
-
+            if (IsOn && CurrentVolume < 10)
+            {
+                
+                CurrentVolume += 1;
+            }
         }
 
         public void LowerVolume()
         {
-
+            if (IsOn && CurrentVolume > 10)
+            {
+                CurrentVolume -= 1;
+            }
         }
 
 
