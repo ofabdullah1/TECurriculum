f3841828f4d62aafb2fdac28482e22bfc2528657
John Fulton <john@techelevator.com>
2022-09-28 15:41:44 -0400

    module-1/14_Unit_Testing/

---
+++ module-1/14_Unit_Testing/exercise/Exercises/NonStart.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercises
+{
+    public class NonStart
+    {
+        /*
+        Given 2 strings, return their concatenation, except omit the first char of each. The strings will
+        be at least length 1.
+        GetPartialString("Hello", "There") → "ellohere"
+        GetPartialString("java", "code") → "avaode"
+        GetPartialString("shotl", "java") → "hotlava"
+        */
+        public string GetPartialString(string a, string b)
+        {
+            if (a == null)
+            {
+                a = "";
+            }
+            if (b == null)
+            {
+                b = "";
+            }
+            if (a.Length == 0 && b.Length > 0)
+            {
+                return b.Substring(1);
+            }
+            else if (b.Length == 0 && a.Length > 0)
+            {
+                return a.Substring(1);
+            }
+            else if (a.Length == 0 && b.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                return a.Substring(1) + b.Substring(1);
+            }
+        }
+    }
+}
