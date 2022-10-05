cbc2b3993ba45bedfd110e35d9723c8841bfa096
John Fulton <john@techelevator.com>
2022-10-05 13:11:28 -0400

    module-1/18_Review_Day/ with in class changes

---
+++ module-1/18_Review_Day/PetInfo-with-johns-changes/PetInfoTest/Classes/PetTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetInfo;
+
+namespace PetInfoTest
+{
+    [TestClass]
+    public class PetTest
+    {
+        Pet pet;
+
+        [TestInitialize]
+        public void Setup()
+        {
+           pet = new Pet(999, "Bobo", "snake", "Blacksnake");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            //no cleanup needed - you could leave this method out
+        }
+
+        [TestMethod]
+        public void PetConstructor()
+        {
+            Assert.IsNotNull(pet);
+        }
+
+        [TestMethod]
+        public void PetProperties()
+        {
+            Assert.AreEqual(999, pet.Id, "Id failed");
+            Assert.AreEqual("Bobo", pet.Name, "Name failed");
+            Assert.AreEqual("snake", pet.Type, "Type failed");
+            Assert.AreEqual("Blacksnake", pet.Breed, "Breed failed");
+        }
+    }
+}
