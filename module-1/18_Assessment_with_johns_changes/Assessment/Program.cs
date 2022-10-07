20ad39878996105e7c73dcbdefcdfd96d6bd5bf7
John Fulton <john@techelevator.com>
2022-10-07 13:54:58 -0400

    module-2/01_Intro_to_Databases/

---
+++ module-1/18_Assessment_with_johns_changes/Assessment/Program.cs
@@ -0,0 +1,21 @@
+﻿using Assessment.Classes;
+using System;
+
+namespace Assessment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // create an object and call methods on it (manual testing) from this class.
+            // You DO NOT need to prompt the user to type in any values
+
+            Ticket myTicket = new Ticket("john", 2);
+
+            decimal myPrice = myTicket.TotalPrice(false, false);
+
+
+            Console.WriteLine(myTicket.ToString());
+        }
+    }
+}
