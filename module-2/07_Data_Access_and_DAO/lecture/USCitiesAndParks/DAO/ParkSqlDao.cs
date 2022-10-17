1fd5862446970402df7f67e72abdfc96d6d605d4
John Fulton <john@techelevator.com>
2022-10-17 15:25:57 -0400

    module-2/07_Data_Access_and_DAO/

---
+++ module-2/07_Data_Access_and_DAO/lecture/USCitiesAndParks/DAO/ParkSqlDao.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using USCitiesAndParks.Models;
+
+namespace USCitiesAndParks.DAO
+{
+    public class ParkSqlDao : IParkDao
+    {
+        private readonly string connectionString;
+
+        public ParkSqlDao(string connString)
+        {
+            connectionString = connString;
+        }
+
+        public Park GetPark(int parkId)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public IList<Park> GetParksByState(string stateAbbreviation)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public Park CreatePark(Park park)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePark(Park park)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public void DeletePark(int parkId)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public void AddParkToState(int parkId, string state_abbreviation)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        public void RemoveParkFromState(int parkId, string state_abbreviation)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+
+        private Park CreateParkFromReader(SqlDataReader reader)
+        {
+            //todo not done
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ module-2/07_Data_Access_and_DAO/lecture/USCitiesAndParks/Program.cs
@@ -0,0 +1,26 @@
+﻿using USCitiesAndParks.DAO;
+
+namespace USCitiesAndParks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IConfigurationBuilder builder = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            //IConfigurationRoot configuration = builder.Build();
+            //string connectionString = configuration.GetConnectionString("UnitedStates");
+
+            string connectionString = @"Server=.\SQLEXPRESS;Database=UnitedStates;Trusted_Connection=True;";
+
+            ICityDao cityDao = new CitySqlDao(connectionString);
+            IStateDao stateDao = new StateSqlDao(connectionString);
+            IParkDao parkDao = new ParkSqlDao(connectionString);
+
+            USCitiesAndParksCLI cli = new USCitiesAndParksCLI(cityDao, stateDao, parkDao);
+            cli.RunCLI();
+        }
+    }
+}
