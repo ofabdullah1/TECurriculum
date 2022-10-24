a54ba0c8f84d294cd5c2b297b6aa6f5e4b93f619
John Fulton <john@techelevator.com>
2022-10-24 14:04:47 -0400

    module-2/12_Consuming_RESTful_APIs_Part_2/ tutorial

---
+++ module-2/12_Consuming_RESTful_APIs_Part_2/tutorial-final/client/LocationClient/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LocationClient
+{
+    class Program
+    {
+        private const string ApiUrl = "http://localhost:3000/";
+        static void Main()
+        {
+            LocationApp app = new LocationApp(ApiUrl);
+            app.Run();
+        }
+    }
+}
