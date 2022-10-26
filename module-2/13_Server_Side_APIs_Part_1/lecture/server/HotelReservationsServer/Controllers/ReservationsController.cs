947d813910594ae291ddbd093720876dc731beed
John Fulton <john@techelevator.com>
2022-10-26 06:31:46 -0400

    module-2/13_Server_Side_APIs_Part_1/

---
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/HotelReservationsServer/Controllers/ReservationsController.cs
@@ -0,0 +1,27 @@
+﻿using HotelReservations.DAO;
+using HotelReservations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HotelReservations.Controllers
+{
+    [Route("reservations")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private static IReservationDao reservationDao;
+        private static IHotelDao hotelDao;
+        public ReservationsController()
+        {
+            if (hotelDao == null)
+            {
+                hotelDao = new HotelMemoryDao();
+            }
+            if (reservationDao == null)
+            {
+                reservationDao = new ReservationMemoryDao();
+            }
+        }
+
+    }
+}
