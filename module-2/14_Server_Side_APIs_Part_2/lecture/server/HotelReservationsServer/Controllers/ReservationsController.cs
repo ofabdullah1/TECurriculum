7996ba591e52d7ca722254f1ba9e1c22a9047464
John Fulton <john@techelevator.com>
2022-10-26 16:26:06 -0400

    module-2/14_Server_Side_APIs_Part_2/

---
+++ module-2/14_Server_Side_APIs_Part_2/lecture/server/HotelReservationsServer/Controllers/ReservationsController.cs
@@ -0,0 +1,59 @@
+﻿using HotelReservations.DAO;
+using HotelReservations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HotelReservations.Controllers
+{
+    [Route("reservations")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private IReservationDao reservationDao;
+        private IHotelDao hotelDao;
+        public ReservationsController(IHotelDao hotelDao, IReservationDao reservationDao)
+        {
+            this.hotelDao = hotelDao;
+            this.reservationDao = reservationDao;
+        }
+
+        [HttpGet()]
+        public List<Reservation> ListReservations()
+        {
+            return reservationDao.List();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> GetReservation(int id)
+        {
+            Reservation reservation = reservationDao.Get(id);
+
+            if (reservation != null)
+            {
+                return reservation;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("/hotels/{hotelId}/reservations")]
+        public ActionResult<List<Reservation>> ListReservationsByHotel(int hotelId)
+        {
+            Hotel hotel = hotelDao.Get(hotelId);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return reservationDao.FindByHotel(hotelId);
+        }
+
+        [HttpPost()]
+        public ActionResult<Reservation> AddReservation(Reservation reservation)
+        {
+            Reservation added = reservationDao.Create(reservation);
+            return Created($"/reservations/{added.Id}", added);
+        }
+    }
+}
