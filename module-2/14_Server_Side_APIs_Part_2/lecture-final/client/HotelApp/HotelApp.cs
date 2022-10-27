029c77783210a11a91cebb57e6b805a5634b8dc8
John Fulton <john@techelevator.com>
2022-10-27 13:50:12 -0400

    module-2/14_Server_Side_APIs_Part_2/ in-class changes

---
+++ module-2/14_Server_Side_APIs_Part_2/lecture-final/client/HotelApp/HotelApp.cs
@@ -0,0 +1,221 @@
+﻿using HotelReservationsClient.Models;
+using HotelReservationsClient.Services;
+using System;
+using System.Collections.Generic;
+
+namespace HotelReservationsClient
+{
+    public class HotelApp
+    {
+        private readonly HotelApiService hotelApiService;
+        private readonly HotelConsoleService console = new HotelConsoleService();
+
+        public HotelApp(string apiURL)
+        {
+            this.hotelApiService = new HotelApiService(apiURL);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                console.PrintMainMenu();
+                int menuSelection = console.PromptForInteger("Please choose an option", 0, 5);
+
+                if (menuSelection == 0)
+                {
+                    // Exit the loop
+                    break;
+                }
+
+                if (menuSelection == 1)
+                {
+                    // List hotels
+                    ShowHotels();
+                }
+
+                if (menuSelection == 2)
+                {
+                    // List Reservations for Hotel
+                    ShowHotelReservations();
+                }
+
+                if (menuSelection == 3)
+                {
+                    // Create new Reservation for Hotel
+                    AddReservation();
+                }
+
+                if (menuSelection == 4)
+                {
+                    // Update existing Reservation for Hotel
+                    UpdateReservation();
+                }
+
+                if (menuSelection == 5)
+                {
+                    // Delete Reservation
+                    DeleteReservation();
+                }
+
+            }
+        }
+
+        private void ShowHotels()
+        {
+            try
+            {
+                List<Hotel> hotels = hotelApiService.GetHotels();
+                if (hotels != null)
+                {
+                    console.PrintHotels(hotels);
+                }
+            }
+            catch (Exception ex)
+            {
+                console.PrintError(ex.Message);
+            }
+            console.Pause();
+        }
+
+        private void ShowHotelReservations()
+        {
+            try
+            {
+                List<Hotel> hotels = hotelApiService.GetHotels();
+                if (hotels != null && hotels.Count > 0)
+                {
+                    int hotelId = console.PromptForHotelId(hotels, "list reservations");
+                    if (hotelId == 0)
+                    {
+                        // User cancel
+                        return;
+                    }
+
+                    // Try to get reservations
+                    List<Reservation> reservations = hotelApiService.GetReservations(hotelId);
+                    if (reservations != null)
+                    {
+                        console.PrintReservations(reservations, hotelId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                console.PrintError(ex.Message);
+            }
+            console.Pause();
+        }
+        private void AddReservation()
+        {
+            try
+            {
+                // Get the Hotel first
+                List<Hotel> hotels = hotelApiService.GetHotels();
+                int hotelId = console.PromptForHotelId(hotels, "create reservation");
+                if (hotelId == 0)
+                {
+                    // User cancel
+                    return;
+                }
+                Reservation reservationToAdd = console.PromptForReservationData(hotelId);
+                if (reservationToAdd == null)
+                {
+                    // user cancel
+                    return;
+                }
+                if (reservationToAdd.IsValid)
+                {
+                    Reservation addedReservation = hotelApiService.AddReservation(reservationToAdd);
+                    if (addedReservation != null)
+                    {
+                        console.PrintSuccess("Reservation successfully added.");
+                    }
+                    else
+                    {
+                        console.PrintError("Reservation not added.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                console.PrintError(ex.Message);
+            }
+            console.Pause();
+        }
+
+        private void UpdateReservation()
+        {
+            // Update an existing reservation
+            try
+            {
+                List<Reservation> reservations = hotelApiService.GetReservations();
+                if (reservations != null)
+                {
+                    int reservationId = console.PromptForReservationId(reservations, "update");
+                    if (reservationId == 0)
+                    {
+                        // User cancel
+                        return;
+                    }
+                    Reservation oldReservation = hotelApiService.GetReservation(reservationId);
+                    if (oldReservation != null)
+                    {
+                        Reservation reservationToUpdate = console.PromptForReservationData(oldReservation.HotelId, oldReservation);
+
+                        if (reservationToUpdate.IsValid)
+                        {
+                            Reservation updatedReservation = hotelApiService.UpdateReservation(reservationToUpdate);
+                            if (updatedReservation != null)
+                            {
+                                console.PrintSuccess("Reservation successfully updated.");
+                            }
+                            else
+                            {
+                                console.PrintError("Reservation not updated.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                console.PrintError(ex.Message);
+            }
+            console.Pause();
+        }
+
+        private void DeleteReservation()
+        {
+            // Delete reservation
+            try
+            {
+                List<Reservation> reservations = hotelApiService.GetReservations();
+                if (reservations != null)
+                {
+                    int reservationId = console.PromptForReservationId(reservations, "delete");
+                    if (reservationId == 0)
+                    {
+                        // User cancel
+                        return;
+                    }
+
+                    bool deleteSuccess = hotelApiService.DeleteReservation(reservationId);
+                    if (deleteSuccess)
+                    {
+                        console.PrintSuccess("Reservation successfully deleted.");
+                    }
+                    else
+                    {
+                        console.PrintError("Reservation not deleted.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                console.PrintError(ex.Message);
+            }
+            console.Pause();
+        }
+    }
+}
---
+++ module-2/14_Server_Side_APIs_Part_2/lecture-with-johns-changes/client/HotelApp/Services/HotelApiService.cs
@@ -0,0 +1,120 @@
+﻿using HotelReservationsClient.Models;
+using HotelReservationsClient.Utility;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace HotelReservationsClient.Services
+{
+    public class HotelApiService
+    {
+        protected static RestClient client = null;
+
+        public HotelApiService(string apiUrl)
+        {
+            if (client == null)
+            {
+                client = new RestClient(apiUrl);
+            }
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            RestRequest request = new RestRequest("hotels");
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+
+            CheckForError(response, "Get hotels");
+            return response.Data;
+        }
+
+        public List<Reservation> GetReservations(int hotelId = 0)
+        {
+            string url;
+            if (hotelId != 0)
+            {
+                url = $"hotels/{hotelId}/reservations";
+            }
+            else
+            { 
+                url = "reservations";
+            }
+
+            RestRequest request = new RestRequest(url);
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+
+            CheckForError(response, $"Get reservations for hotel {hotelId}");
+            return response.Data;
+        }
+
+        public Reservation GetReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationId}");
+            IRestResponse<Reservation> response = client.Get<Reservation>(request);
+
+            CheckForError(response, $"Get reservation {reservationId}");
+            return response.Data;
+        }
+
+        public Reservation AddReservation(Reservation newReservation)
+        {
+            RestRequest request = new RestRequest("reservations");
+            request.AddJsonBody(newReservation);
+            IRestResponse<Reservation> response = client.Post<Reservation>(request);
+
+            CheckForError(response, "Add reservation");
+            return response.Data;
+        }
+
+        public Reservation UpdateReservation(Reservation reservationToUpdate)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationToUpdate.Id}");
+            request.AddJsonBody(reservationToUpdate);
+            IRestResponse<Reservation> response = client.Put<Reservation>(request);
+
+            CheckForError(response, $"Update reservation {reservationToUpdate.Id}");
+            return response.Data;
+        }
+
+        public bool DeleteReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationId}");
+            IRestResponse response = client.Delete(request);
+
+            CheckForError(response, $"Delete reservation {reservationId}");
+            return true;
+        }
+
+        /// <summary>
+        /// Checks RestSharp response for errors. If error, writes a log message and throws an exception 
+        /// if the call was not successful. If no error, just returns to caller.
+        /// </summary>
+        /// <param name="response">Response returned from a RestSharp method call.</param>
+        /// <param name="action">Description of the action the application was taking. Written to the log file for context.</param>
+        private void CheckForError(IRestResponse response, string action)
+        {
+            string message = "";
+            string messageDetails = "";
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                message = $"Error occurred in '{action}' - unable to reach server.";
+                messageDetails = $"Action: {action}\n" +
+                    $"\tResponse status was '{response.ResponseStatus}'.";
+                if (response.ErrorException != null)
+                {
+                    messageDetails += $"\n\t{response.ErrorException.Message}";
+                }
+            }
+            else if (!response.IsSuccessful)
+            {
+                message = $"An http error occurred.";
+                messageDetails = $"Action: {action}\n" +
+                    $"\tResponse: {(int)response.StatusCode} {response.StatusDescription}";
+            }
+            if (message.Length > 0)
+            {
+                BasicLogger.Log($"{message}\n\t{messageDetails}\n");
+                throw new HttpRequestException(message, response.ErrorException);
+            }
+        }
+    }
+}
