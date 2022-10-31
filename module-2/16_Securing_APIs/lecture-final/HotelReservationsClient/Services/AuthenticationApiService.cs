4311160c09cec1ad33afb1705b7cf5d89d8e5c4a
John Fulton <john@techelevator.com>
2022-10-31 13:44:43 -0400

    module-2/16_Securing_APIs in-class changes

---
+++ module-2/16_Securing_APIs/lecture-final/HotelReservationsClient/Services/AuthenticationApiService.cs
@@ -0,0 +1,32 @@
+﻿using HotelReservationsClient.Models;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace HotelReservationsClient.Services
+{
+    public class AuthenticationApiService : ApiService
+    {
+        private static ApiUser user = new ApiUser();
+        public bool LoggedIn { get { return !string.IsNullOrWhiteSpace(user.Token); } }
+        public AuthenticationApiService(string apiUrl) : base(apiUrl) { }
+
+        public bool Login(string submittedName, string submittedPass)
+        {
+            LoginUser loginUser = new LoginUser { Username = submittedName, Password = submittedPass };
+            RestRequest request = new RestRequest("login");
+            request.AddJsonBody(loginUser);
+            IRestResponse<ApiUser> response = client.Post<ApiUser>(request);
+
+            CheckForError(response, "Login");
+            user.Token = response.Data.Token;
+            client.Authenticator = new JwtAuthenticator(user.Token);
+            return true;
+        }
+
+        public void Logout()
+        {
+            user = new ApiUser();
+            client.Authenticator = null;
+        }
+    }
+}
---
+++ module-2/16_Securing_APIs/lecture-final/HotelReservationsClient/Services/HotelApiService.cs
@@ -0,0 +1,77 @@
+﻿using HotelReservationsClient.Models;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace HotelReservationsClient.Services
+{
+    public class HotelApiService : ApiService
+    {
+        public HotelApiService(string apiUrl) : base(apiUrl) { }
+
+        public List<Hotel> GetHotels()
+        {
+            RestRequest request = new RestRequest("hotels");
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+
+            CheckForError(response, "Get hotels");
+            return response.Data;
+        }
+
+        public List<Reservation> GetReservations(int hotelId = 0)
+        {
+            string url;
+            if (hotelId != 0)
+            {
+                url = $"hotels/{hotelId}/reservations";
+            }
+            else
+            { 
+                url = "reservations";
+            }
+
+            RestRequest request = new RestRequest(url);
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+
+            CheckForError(response, $"Get reservations for hotel {hotelId}");
+            return response.Data;
+        }
+
+        public Reservation GetReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationId}");
+            IRestResponse<Reservation> response = client.Get<Reservation>(request);
+
+            CheckForError(response, $"Get reservation {reservationId}");
+            return response.Data;
+        }
+
+        public Reservation AddReservation(Reservation newReservation)
+        {
+            RestRequest request = new RestRequest("reservations");
+            request.AddJsonBody(newReservation);
+            IRestResponse<Reservation> response = client.Post<Reservation>(request);
+
+            CheckForError(response, "Add reservation");
+            return response.Data;
+        }
+
+        public Reservation UpdateReservation(Reservation reservationToUpdate)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationToUpdate.Id}");
+            request.AddJsonBody(reservationToUpdate);
+            IRestResponse<Reservation> response = client.Put<Reservation>(request);
+
+            CheckForError(response, $"Update reservation {reservationToUpdate.Id}");
+            return response.Data;
+        }
+
+        public bool DeleteReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest($"reservations/{reservationId}");
+            IRestResponse response = client.Delete(request);
+
+            CheckForError(response, $"Delete reservation {reservationId}");
+            return true;
+        }
+    }
+}
