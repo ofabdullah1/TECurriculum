c5920477a1d2188b5464c285df6e3c057ea69972
John Fulton <john@techelevator.com>
2022-10-31 14:10:17 -0400

    module-2/17_Review_Day/

---
+++ module-2/17_Review_Day/PetInfo/PetInfoServer/Controllers/PetController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetInfoClient.Models;
+using PetInfoServer.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetInfoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private IPetDAO petDAO;
+
+        public PetController(IPetDAO petDAO)
+        {
+            this.petDAO = petDAO;
+        }
+
+        // GET /pet
+        [HttpGet]
+        public ActionResult<List<Pet>> getPets()
+        {
+            return Ok(petDAO.GetPets());
+        }
+
+        // DELETE /pet/3
+        [HttpDelete("{petId}")]
+        public ActionResult deletePet(int petId)
+        {
+            bool result = petDAO.DeletePet(petId);
+                if (result)
+            {
+                return Ok();
+            }
+                else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
