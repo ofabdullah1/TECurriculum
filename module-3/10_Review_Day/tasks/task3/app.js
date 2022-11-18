c5a6eafbb3c028b6b61d7628f3653cf804f6e3a4
Omar Abdullah <ofabdullah1@gmail.com>
2022-11-18 12:50:08 -0500

    task 3 NOT complete

--- module-3/10_Review_Day/tasks/task3/app.js
+++ module-3/10_Review_Day/tasks/task3/app.js
@@ -1,2 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    DisplayDate();
+    
+
+    document.getElementsByClassName('btn').addEventListener('click', DisplayDate())
+
+
+    function DisplayDate() {
+      document.getElementsByClassName("btn").innerHTML = Date();
+    }
+
+})
 
 
