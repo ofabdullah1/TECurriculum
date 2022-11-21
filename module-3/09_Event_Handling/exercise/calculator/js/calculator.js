4c7dbab5949a916aaa006f10fea1ec41bdf0eabf
Omar Abdullah <ofabdullah1@gmail.com>
2022-11-21 08:00:46 -0500

    now that some test are passing will come back

--- module-3/09_Event_Handling/exercise/calculator/js/calculator.js
+++ module-3/09_Event_Handling/exercise/calculator/js/calculator.js
@@ -76,16 +76,26 @@ document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
 
   // set the variable called display declared at the top of this file equal to the display element
   // HINT: use its id #display to get a reference to it
-
+  display = document.getElementById('display');
   // get a reference to all of the numbers
   // loop over each of the numbers
   // add a click event listener to each number to call the function clickNumber
-
+const numbers = document.querySelectorAll('.decimal')
+numbers.forEach((number) => {
+  number.addEventListener('click', (event) => {
+    clickNumber(event);
+  })
+})
   // get a reference to the decimal point button
   // add a click event listener to call the function clickNumber
   // the decimal point is part of the number so you can call clickNumber for it 
   // as you would for a number
-
+  const decimal = document.querySelectorAll('.number')
+  numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+      clickOperator(event);
+    })
+  })
   // get a reference to the all clear button
   // add a click event listener to call the function clear  
 
@@ -95,6 +105,11 @@ document.addEventListener('LOADED_EVENT_GOES_HERE', () => {
 
   // add click event listener for the equal sign
   // should call the function performOperation
+  const equals = document.querySelectorAll('.equal-sign')
+  numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+      performOperation(event);
+    })
 
 });
 
