f7f4b1e53c5b62b71bfdaab582424388254bf35d
John Fulton <john@techelevator.com>
2022-11-28 13:57:34 -0500

    module-3/16_Vue_Router/ in-class changes

---
+++ module-3/16_Vue_Router/johns-in-class-example/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Page not found. Please try again</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/16_Vue_Router/lecture-with-johns-changes/src/views/ProductDetail.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>Product Detail</h1>
+  <h2>{{productId}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      productId: 0,
+    };
+  },
+  created(){
+      this.productId = this.$route.params.productId;
+      console.log("reached created of ProductDetail.vue")
+  }
+};
+</script>
+
+<style>
+</style>
