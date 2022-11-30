95b71c68c2cf736d35126070277838740319d502
John Fulton <john@techelevator.com>
2022-11-29 21:48:46 -0500

    module-3/17_Web_Services/

---
+++ module-3/17_Web_Services/pet-info/vue/src/components/CustomerDetail.vue
@@ -0,0 +1,54 @@
+<template>
+  <table class="table table-bordered table-hover">
+    <tbody>
+      <tr>
+        <td>Name</td>
+        <td>{{ customer.name }}</td>
+      </tr>
+
+      <tr>
+        <td>Phone</td>
+        <td>{{ customer.phone }}</td>
+      </tr>
+
+      <tr>
+        <td>Email</td>
+        <td>{{ customer.email }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "CustomerDetail",
+
+  data: function () {
+    return {
+      customer: {},
+    };
+  },
+
+  props: ["id"],
+
+  created() {
+    let customers = this.$store.state.customers;
+    this.customer = customers.find((item) => {
+      return item.id == this.id;
+    });
+  },
+};
+</script>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid black;
+}
+
+table {
+  width: 50%;
+  margin: 20px;
+}
+</style>
---
+++ module-3/17_Web_Services/pet-info/vue/src/views/Pet.vue
@@ -0,0 +1,27 @@
+<template>
+  <pet-detail v-bind:id="petNumber" />
+</template>
+
+<script>
+import PetDetail from "../components/PetDetail.vue";
+export default {
+  name: 'Pet',
+  
+  components: {
+    PetDetail,
+  },
+
+    data() {
+    return {
+      petNumber: 0,
+    }
+  },
+
+  created() {
+      this.petNumber = this.$route.params.id;
+  },
+};
+</script>
+
+<style>
+</style>
